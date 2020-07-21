b54cda34128c5b4afc6d9926bca5ee9feecfd253
yunis.hawash <yunis.hawash@gmail.com>
2020-07-21 03:01:03 +0300

    [1]- adding server.js file for express server to lunch the application, [2]- removing the build folder from .gitignore file to upload it into the repo

---
+++ build/precache-manifest.3f7bfd608dad81eced92fff694e06683.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24c2672e98681c457757e0394365df65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e50985493739dc3ce6c",
+    "url": "/static/js/0.7e6bc35d.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/static/js/0.7e6bc35d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c5a9d721bdbd78b48e4",
+    "url": "/static/js/10.3191f75d.chunk.js"
+  },
+  {
+    "revision": "f310745c86949c292ec4",
+    "url": "/static/js/4.fc3ee2d2.chunk.js"
+  },
+  {
+    "revision": "9ddf9e78bec413bb09f6",
+    "url": "/static/js/5.1e277fcc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/5.1e277fcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6805c25085269f5bbd82",
+    "url": "/static/js/6.f346c486.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/static/js/6.f346c486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3b0a9a3825641e07f86",
+    "url": "/static/js/7.8d156e00.chunk.js"
+  },
+  {
+    "revision": "96c73944233a509a3aab13867e8f94e4",
+    "url": "/static/js/7.8d156e00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5560b9876f5bff371db0",
+    "url": "/static/js/8.be3964f4.chunk.js"
+  },
+  {
+    "revision": "72d5df1e32bc3f05c62d",
+    "url": "/static/js/9.fb968b9e.chunk.js"
+  },
+  {
+    "revision": "03b33f21a2356157a426",
+    "url": "/static/js/main.f0f95913.chunk.js"
+  },
+  {
+    "revision": "c2e109c2524dba0ef560",
+    "url": "/static/js/runtime-main.ba52bea0.js"
+  },
+  {
+    "revision": "cfb1c8616d2901f9104d",
+    "url": "/static/js/xlsx.0ef57332.chunk.js"
+  }
+]);
---
+++ build/static/js/xlsx.0ef57332.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpcovid-19-meter"]=this["webpackJsonpcovid-19-meter"]||[]).push([[3],{264:function(n,c){},375:function(n,c){},376:function(n,c){}}]);
+//# sourceMappingURL=xlsx.0ef57332.chunk.js.map
